e8eae118c4991a2fdde7c84da813c895d5aba8f8
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-09-22 15:03:07 +0300

    додає другий варіант вирішення

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -45,3 +45,76 @@ function addContentForm() {
     // textareaRef.value = parseValueKey.message;
   }
 }
+
+///////////////////////////////////////
+// ('use strict');
+// import throttle from 'lodash.throttle';
+// const formRef = document.querySelector('.js-contact-form');
+// const LOCALE_STORAGE_KEY = 'contact-form-key';
+// import { save, load, remove } from './storage';
+
+// initPage();
+
+// const onFormInput = event => {
+//   const { name, value } = event.target;
+
+//   let saveData = load(LOCALE_STORAGE_KEY);
+//   saveData = saveData ? saveData : {};
+
+//   saveData[name] = value;
+
+//   save(LOCALE_STORAGE_KEY, saveData);
+// };
+
+// const throttledOnFormInput = throttle(onFormInput, 300);
+// formRef.addEventListener('input', throttledOnFormInput);
+
+// function initPage() {
+//   const saveData = load(LOCALE_STORAGE_KEY);
+
+//   if (!saveData) {
+//     return;
+//   }
+//   Object.entries(saveData).forEach(([name, value]) => {
+//     formRef.elements[name].value = value;
+//   });
+// }
+
+// const handleSubmit = event => {
+//   event.preventDefault();
+
+//   const {
+//     elements: { name, email, message },
+//   } = event.currentTarget;
+
+//   console.log({ name: name.value, email: email.value, message: message.value });
+//   event.currentTarget.reset();
+//   remove(LOCALE_STORAGE_KEY);
+// };
+
+// formRef.addEventListener('submit', handleSubmit);
+
+// export const save = (key, value) => {
+//   try {
+//     const serializedState = JSON.stringify(value);
+//     localStorage.setItem(key, serializedState);
+//   } catch (error) {
+//     console.error('Set state error: ', error.message);
+//   }
+// };
+
+// export const load = key => {
+//   try {
+//     const serializedState = localStorage.getItem(key);
+//     return serializedState === null ? undefined : JSON.parse(serializedState);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// };
+// export const remove = key => {
+//   try {
+//     localStorage.removeItem(key);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// };
